1718deb4f6a4cc789b06b0fc0846bb8b8afa69fd
Andrew Burton <tuglyraisin@gmail.com>
2022-05-05 19:47:32 -0500

    Initial commit

---
+++ Payday/Payday.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace Payday
+{
+    public class Payday
+    {
+        private int _endOfFirstPayPeriod = 15;
+        private DateTime _currentDay;
+
+        public DateTime CurrentDay
+        {
+            get
+            {
+                return _currentDay;
+            }
+        }
+
+        public int DaysToPayday
+        {
+            get
+            {
+                TimeSpan DaysUntilPayday = NextPayday() - CurrentDay;
+                return DaysUntilPayday.Days;
+            }
+        }
+
+        public int DaysToEndOfMonth
+        {
+            get
+            {
+                return GetLastDayOfMonth() - CurrentDay.Day;
+            }
+        }
+
+        private bool IsWeekday(DateTime CheckDay)
+        {
+            if ((int)CheckDay.DayOfWeek > 0 && (int)CheckDay.DayOfWeek < 6)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private int GetLastDayOfMonth()
+        {
+            DateTime CheckDay = CurrentDay;
+            int CurrentMonth = CurrentDay.Month;
+            int LastDay = 0;
+
+            while (CurrentMonth == CheckDay.Month)
+            {
+                LastDay = CheckDay.Day;
+                CheckDay = CheckDay.AddDays(1.0);
+            }
+
+            return LastDay;
+        }
+
+        private DateTime _GetPaydayFromPayPeriodEnd(DateTime CheckDay)
+        {
+            while (!IsWeekday(CheckDay))
+            {
+                CheckDay = CheckDay.AddDays(-1.0);
+            }
+
+            return CheckDay;
+        }
+
+        public DateTime GetEndMonthPayday()
+        {
+            return _GetPaydayFromPayPeriodEnd(new DateTime(CurrentDay.Year, CurrentDay.Month, GetLastDayOfMonth()));
+        }
+
+        public DateTime GetMidMonthPayday()
+        {
+            return _GetPaydayFromPayPeriodEnd(new DateTime(CurrentDay.Year, CurrentDay.Month, _endOfFirstPayPeriod));
+        }
+
+        public DateTime GetFirstPaydayOfNextMonth()
+        {
+            return _GetPaydayFromPayPeriodEnd(new DateTime(CurrentDay.Year, CurrentDay.Month, _endOfFirstPayPeriod).AddMonths(1));
+        }
+
+        public DateTime NextPayday()
+        {
+            DateTime CheckDay = CurrentDay;
+            if (CheckDay < GetMidMonthPayday())
+            {
+                return GetMidMonthPayday();
+            }
+            else if (CheckDay < GetEndMonthPayday())
+            {
+                return GetEndMonthPayday();
+            }
+            else
+            {
+                return GetFirstPaydayOfNextMonth();
+            }
+        }
+
+        public void NextDay(Double advance = 1.0)
+        {
+            _currentDay = _currentDay.AddDays(advance);            
+        }
+
+        public Payday(DateTime theCurrentDay)
+        {
+            _currentDay = theCurrentDay;
+        }
+
+        public Payday()
+        {
+            _currentDay = DateTime.Now;
+        }
+    }
+}
---
+++ PaydayGui/Paydate.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Payday;
+
+namespace PaydayGui
+{
+    class Paydate : INotifyPropertyChanged
+    {
+        private Payday.Payday _payday;
+        private DateTime _paydate;
+
+        public DateTime Today
+        {
+            get
+            {
+                return _payday.CurrentDay;
+            }
+        }
+
+        public int DaysToTheNextPayday
+        {
+            get
+            {
+                return _payday.DaysToPayday;
+            }
+        }
+
+        public string DayAndMonthOfNow
+        {
+            get
+            {
+                return String.Format("{0:D2}-{1:D2}-{2:D2}", _payday.CurrentDay.Year, _payday.CurrentDay.Month, _payday.CurrentDay.Day);
+            }
+        }
+
+        public string DayAndMonthOfPay
+        {
+            get 
+            {
+                return String.Format("{0:D2}-{1:D2}-{2:D2}", _paydate.Year, _paydate.Month, _paydate.Day);
+            }
+        }
+
+        public void Next()
+        {
+            _payday.NextDay();
+            _paydate = _payday.NextPayday();
+            NotifyPropertyChanged("Today");
+            NotifyPropertyChanged("Paydate");
+            NotifyPropertyChanged("DaysToTheNextPayday");
+            NotifyPropertyChanged("DayAndMonthOfNow");
+            NotifyPropertyChanged("DayAndMonthOfPay");
+        }
+
+        public Paydate()
+        {
+            _payday = new Payday.Payday(DateTime.Now);
+            _paydate = _payday.NextPayday();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
